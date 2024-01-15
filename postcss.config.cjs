91308df02d0461018149db4691a3f3c2d015f742
H-ymt <y.handai1272@gmail.com>
2024-01-15 23:38:20 +0900

    feat: setting linter and formatter

---
+++ postcss.config.cjs
@@ -0,0 +1,12 @@
+module.exports = () => ({
+  plugins: [
+    require("autoprefixer")({}),
+    require("postcss-sort-media-queries")({
+      sort: "mobile-first",
+    }),
+    require("postcss-url")({
+      url: "inline",
+      maxSize: 150, //インラインにする画像ファイルの容量上限
+    }),
+  ],
+});
