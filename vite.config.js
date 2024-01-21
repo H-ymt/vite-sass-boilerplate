3a0858c0102a870b805ee621f38868383801993f
H-ymt <y.handai1272@gmail.com>
2024-01-22 01:43:17 +0900

    build(prettier): update prettier rule

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 //import設定を追記
-import { resolve } from "path";
+import { resolve } from 'path';
 
 // HTMLの複数出力を自動化する
 //./src配下のファイル一式を取得
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
 
 const files = [];
 function readDirectory(dirPath) {
@@ -17,40 +17,40 @@ function readDirectory(dirPath) {
 
     if (fs.statSync(itemPath).isDirectory()) {
       // componentsディレクトリを除外する
-      if (item === "components") {
+      if (item === 'components') {
         continue;
       }
 
       readDirectory(itemPath);
     } else {
       // htmlファイル以外を除外する
-      if (path.extname(itemPath) !== ".html") {
+      if (path.extname(itemPath) !== '.html') {
         continue;
       }
 
       // nameを決定する
       let name;
-      if (dirPath === path.resolve(__dirname, "src")) {
+      if (dirPath === path.resolve(__dirname, 'src')) {
         name = path.parse(itemPath).name;
       } else {
-        const relativePath = path.relative(path.resolve(__dirname, "src"), dirPath);
-        const dirName = relativePath.replace(/\//g, "_");
+        const relativePath = path.relative(path.resolve(__dirname, 'src'), dirPath);
+        const dirName = relativePath.replace(/\//g, '_');
         name = `${dirName}_${path.parse(itemPath).name}`;
       }
 
       // pathを決定する
-      const relativePath = path.relative(path.resolve(__dirname, "src"), itemPath);
+      const relativePath = path.relative(path.resolve(__dirname, 'src'), itemPath);
       const filePath = `/${relativePath}`;
 
       files.push({ name, path: filePath });
     }
   }
 }
-readDirectory(path.resolve(__dirname, "src"));
+readDirectory(path.resolve(__dirname, 'src'));
 const inputFiles = {};
 for (let i = 0; i < files.length; i++) {
   const file = files[i];
-  inputFiles[file.name] = resolve(__dirname, "./src" + file.path);
+  inputFiles[file.name] = resolve(__dirname, './src' + file.path);
 }
 /*
   この形を自動的に作る
@@ -61,9 +61,9 @@ for (let i = 0; i < files.length; i++) {
 */
 
 export default defineConfig({
-  root: "./src", //開発ディレクトリ設定
+  root: './src', //開発ディレクトリ設定
   build: {
-    outDir: "../dist", //出力場所の指定
+    outDir: '../dist', //出力場所の指定
     rollupOptions: {
       //ファイル出力設定
       output: {},
